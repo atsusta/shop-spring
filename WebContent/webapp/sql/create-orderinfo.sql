78402410acec8d8ee666bed63ef77b2e8a5f396d
usta <sc.usta.ved@gmail.com>
2016-07-18 14:35:07 +0900

    added foreign key

--- WebContent/webapp/sql/create-orderinfo.sql
+++ WebContent/webapp/sql/create-orderinfo.sql
@@ -2,8 +2,10 @@ create table order_info(
 	order_no int not null auto_increment primary key,
 	order_date datetime not null,
 	order_quantity int not null,
-	customer_id varchar(30) not null,
-	product_no int not null,
+	customer_id varchar(30) not null
+        FOREIGN KEY REFERENCES customer(id),
+	product_no int not null
+        FOREIGN KEY REFERENCES product(no),
 	product_size varchar(6) not null,
 	payment_type varchar(20) not null,
 	payment_check tinyint(1) not null,
